de1f92d40cd21882d858fe18b44154171e5ffdee
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-15 12:01:40 +0100

    Affichage BDD articles

--- index.js
+++ index.js
@@ -1,91 +1,39 @@
-const express = require('express') // récupération d'express
 require('dotenv').config()
-const app = express() //variable utilisant la librairie récupération d'express
 const mariadb = require('mariadb');
+const express = require('express') //récupération d'express
+const app = express() //variable utilisant librairie express
 let cors = require('cors')
+//const question = require('./question.json')
+
+console.log(process.env.DB_HOST)
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    database : process.env.DB_DTB,
-    user:process.env.DB_USER,
-    password:process.env.DB_PWD,
+    database: process.env.DB_DTB,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    connectLimit:100
 });
-app.get('/question',async(req,res)=>{
+
+app.use(express.json())
+app.use(cors())
+
+//connexion BDD
+app.get('/articles', async(req,res) => {
     let conn;
     try{
-        console.log("lancement de la connexion")
+        console.log("Lancement de la connexion")
         conn = await pool.getConnection();
-        console.log("lancement de la requête")
-        const rows = await conn.query('Select * from question;');
+        console.log("Lancement de la requête")
+        const rows = await conn.query('select * from articles');
         console.log(rows);
-        res.status(200).json(rows)
+        res.status(200).json(rows);
     }
     catch(err){
         console.log(err);
     }
 })
-app.use(express.json())
-app.use(cors())
-//On définit la route racine"/"
-app.get("/", (req,res)=>{
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("<h1> Page d'accueil </h1> "); //On envoie du code HTML
-});
 
-app.get("/1", (req,res)=>{
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("<h1> Page 1 </h1> "); //On envoie du code HTML
-});
 
-app.get("/2", (req,res)=>{
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("<h1> Page 2 </h1> "); //On envoie du code HTML
-});
-
-app.get("/page/:numPage/", (req, res) => {
-    let page= req.params.numPage; //On récupère la variable numPage
-    page=parseInt(page);
-    let valide=Number.isInteger(page);
-    if(valide){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end("<h1> Page " + page +" </h1>");
-    }else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end("<h1> Attention on attends un entier !!! </h1>");
-    }
-    
-});
-
-app.get('/question', (req,res) => {
-    res.status(200).json(question)
+app.listen(3008, () => { //ouverture du serveur sur port 3000
+    console.log("Serveur à l'écoute") //affiche message dans la console
 })
-
-app.get('/question/:id', (req,res)=>{
-    const id = parseInt(req.params.id)
-    const laQuestion = question.find(question=> question.id === id)
-    res.status(200).json(laQuestion)
-})
-
-app.post('/question', (req,res)=>{
-    question.push(req.body)
-    res.status(200).json(question)
-})
-
-app.put('/question/:id', (req,res)=>{
-    const id = parseInt(req.params.id)
-    let laQuestion = question.find(question=> question.id === id)
-    laQuestion.theme=req.body.theme,
-    laQuestion.question=req.body.question,
-    laQuestion.reponse=req.body.reponse,
-    res.status(200).json(laQuestion)
-})
-
-app.delete('/question/:id', (req,res)=>{
-    const id = parseInt(req.params.id)
-    let laQuestion = question.find(question=> question.id === id)
-    question.splice(question.indexOf(laQuestion),1)
-    res.status(200).json(question)
-})
-
-app.listen(8000, () => {
-    console.log('Server started on port 8000');
-  })
