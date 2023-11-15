6548c3df0a553d1be87454cbc7c606ce1f67e6c4
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-15 12:02:56 +0100

    modification accueil
    peut voir bdd articles

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 export default function Accueil() {
+  
+  
+  // Pour stocker
+  const [blog, setblog] = useState([]); 
+  // Pour gérer l'affichage
+  const [affichage, setAffichage] = useState(false);
+
+
+  const recup = async ()=>{
+    //Chargement BDD
+    await fetch(`http://localhost:3008/articles`, 
+    {method: "GET"})
+    .then(reponse => reponse.json()).then(data => {
+        setblog(data);
+        setAffichage(true);
+        console.log(data);
+      })
+      .catch(error => console.error(error));
+  };
+
+
+
+
+
+  useEffect(() => {
+    recup()
+  },[])
+
+  
+
   return (
-    <div>Accueil</div>
+    <div>
+      {affichage ? 
+        blog.map(articles => (
+          <div>
+            <fieldset>
+              <p>Articles : {articles.utilisateurs_id}</p>
+              <p>Articles : {articles.titre}</p>
+            </fieldset>
+          </div>
+        )) : <p>Chargement ...</p>}
+      
+        <br/><br/>
+    </div>
   )
 }
+
