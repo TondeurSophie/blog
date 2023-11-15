318cde1193caf0d10c18c0270cbf244100e7b98b
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-15 11:41:25 +0100

    modification connexion visuel

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../App.css'
 
-export default function Connexion() {
+function Connexion() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    
+  };
+
   return (
-    <div>Connexion</div>
-  )
+    
+    <div className='formulaire'>
+    <center>
+      <h2>Connexion</h2>
+      <form>
+        <label>Email: 
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br/>
+        <label>Mot de passe:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br/>
+        <button type="button" onClick={handleLogin}>Se Connecter</button>
+      </form>
+      </center>
+    </div>
+    
+  );
 }
+
+export default Connexion;
