05620124129c87a08bab9f2093ba5ff6b1ff18eb
Prapaline <124569549+Prapaline@users.noreply.github.com>
2023-11-15 15:12:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,80 +1,111 @@
-const express = require('express') // récupération d'express
-require('dotenv').config()
-const app = express() //variable utilisant la librairie récupération d'express
+const express = require('express');
+require('dotenv').config();
+const app = express();
 const mariadb = require('mariadb');
-let cors = require('cors')
+const cors = require('cors');
+
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    database : process.env.DB_DTB,
-    user:process.env.DB_USER,
-    password:process.env.DB_PWD,
-});
-const article = [];
-app.get('/articles',async(req,res)=>{
-    let conn;
-    try{
-        console.log("lancement de la connexion")
-        conn = await pool.getConnection();
-        console.log("lancement de la requête")
-        const rows = await conn.query('Select * from articles;');
-        console.log(rows);
-        res.status(200).json(rows)
-    }
-    catch(err){
-        console.log(err);
-    }
-})
-app.use(express.json())
-app.use(cors())
-//On définit la route racine"/"
-app.get("/", (req,res)=>{
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("<h1> Page d'accueil </h1> "); //On envoie du code HTML
+    database: process.env.DB_DTB,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
 });
 
+app.use(express.json());
+app.use(cors());
 
-
-app.get('/articles', async(req,res) => {
+app.get('/articles', async (req, res) => {
     let conn;
-    try{
-        console.log("Lancement de la connexion")
+    try {
         conn = await pool.getConnection();
-        console.log("Lancement de la requête")
-        const rows = await conn.query('select * from articles');
-        console.log(rows);
+        const rows = await conn.query('SELECT * FROM articles;');
         res.status(200).json(rows);
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) conn.release();
     }
-})
-
-app.get('/article/:titre', (req, res) => {
-    const titre = req.params.titre; // Utilisez le bon nom de paramètre
-    const larticle = article.find(article => article.titre === titre);
-    res.status(200).json(larticle);
 });
 
-app.post('/article', (req,res)=>{
-    article.push(req.body)
-    res.status(200).json(article)
-})
+app.get('/', (req, res) => {
+    res.status(200).send('<h1> Page d\'accueil </h1>');
+});
 
-app.put('/article/:titre', (req, res) => {
+app.get('/article/:titre', async (req, res) => {
     const titre = req.params.titre;
-    let larticle = article.find(article => article.titre === titre);
-    larticle.theme = req.body.theme;
-    larticle.article = req.body.article;
-    larticle.reponse = req.body.reponse;
-    res.status(200).json(larticle);})
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query('SELECT * FROM articles WHERE titre = ?;', [titre]);
+        if (rows.length > 0) {
+            res.status(200).json(rows[0]);
+        } else {
+            res.status(404).json({ error: 'Article not found' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) conn.release();
+    }
+});
 
-    app.delete('/article/:titre', (req, res) => {
-        const titre = req.params.titre;
-        let larticle = article.find(article => article.titre === titre);
-        article.splice(article.indexOf(larticle), 1);
-        res.status(200).json(article);
-    });
+app.post('/article', async (req, res) => {
+    const nouvelArticle = req.body;
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.query(
+            'INSERT INTO articles (titre, theme, article, reponse) VALUES (?, ?, ?, ?);',
+            [nouvelArticle.titre, nouvelArticle.theme, nouvelArticle.article, nouvelArticle.reponse]
+        );
+        res.status(201).json({ message: 'Article added successfully' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) conn.release();
+    }
+});
 
+app.put('/article/:titre', async (req, res) => {
+    const titre = req.params.titre;
+    const { theme, article, reponse } = req.body;
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.query(
+            'UPDATE articles SET theme = ?, article = ?, reponse = ? WHERE titre = ?;',
+            [theme, article, reponse, titre]
+        );
+        res.status(200).json({ message: 'Article updated successfully' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.delete('/article/:titre', async (req, res) => {
+    const titre = req.params.titre;
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.query('DELETE FROM articles WHERE titre = ?;', [titre]);
+        res.status(200).json({ message: 'Article deleted successfully' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+// Lancement du serveur
 app.listen(8000, () => {
     console.log('Server started on port 8000');
   })
